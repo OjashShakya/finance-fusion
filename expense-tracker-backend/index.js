e3115dca910823587b50846c03f832c280798cb8
OjashShakya <ojashshakya56@gmail.com>
2025-05-11 09:29:41 +0545

    Vercel Deployment Backend Fixed

--- expense-tracker-backend/index.js
+++ expense-tracker-backend/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 // CORS setup
 app.use(cors({
-  origin: ["http://localhost:3000", "http://localhost:3001","http://localhost:3002","http://localhost:3003"],
+  origin: ["http://localhost:3000", "http://localhost:3001","http://localhost:3002","http://localhost:3003","https://financefusion-six.vercel.app"],
   credentials: true,
 }));
 
@@ -43,3 +43,5 @@ app.use("/api", mainRouter);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+
+module.exports = app;
